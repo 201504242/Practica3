4c6f4b672a8c6f5f779fb7c60d4412bc6b0171a7
201504242 <201504242@ingenieria.usac.edu.gt>
2019-04-03 19:14:52 -0600

    Inicio del proyecto

---
+++ SitioWEB/App_Code/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SitioWEB.Startup))]
+namespace SitioWEB
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
