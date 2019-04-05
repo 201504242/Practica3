717a5780be000f69c4247e5cf9118f8d1c20a5bd
fredyramirez <fredyramirezsando@gmail.com>
2019-04-04 20:15:43 -0600

    registrar 201503368

--- BancoGT/BancoGT/Inicio.aspx.cs
+++ BancoGT/BancoGT/Inicio.aspx.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
@@ -9,6 +10,8 @@ namespace BancoGT
 {
     public partial class Inicio : System.Web.UI.Page
     {
+
+        Operaciones op = new Operaciones();
         protected void Page_Load(object sender, EventArgs e)
         {
             System.Web.HttpContext.Current.Session["usuario"] = "USU";
@@ -16,9 +19,62 @@ namespace BancoGT
             System.Web.HttpContext.Current.Session["codigo"] = "----";
         }
 
+        public void CrearUsuario()
+        {
+            /* if (txt_usuario.Text.Equals("") && txt_contrasena.Text.Equals(""))
+             {
+                 MessageBox.Show("Error, los campos deben de estar completos");
+             }
+             else if (txt_usuario.Text.Equals("") && !txt_contrasena.Text.Equals(""))
+             {
+                 MessageBox.Show("Error, el campo usuario no puede ser vacio");
+             }
+             else if (!txt_usuario.Text.Equals("") && txt_contrasena.Text.Equals(""))
+             {
+                 MessageBox.Show("Error, el campo password no puede ser vacio");
+             }*/
+            String usua = txtusuario.Text;
+            String pass = txtcontra.Text;
+            var ArregloPass = pass.ToCharArray();
+            var ArregloUsua = usua.ToCharArray();
+            bool contNum = false;
+            bool contString = false;
+            foreach (char item in txtusuario.Text)
+            {
+                if (Char.IsNumber(item))
+                {
+                    contNum = true;
+                }
+                else if (Char.IsLetter(item))
+                {
+                    contString = true;
+                }
+            }
+            if (!contString || !contNum)
+            {
+                Console.WriteLine("Error, falta numero o letra");
+            }
+            else if (ArregloUsua.Length > 12)
+            {
+                Console.WriteLine("Error");
+            }
+            else if (ArregloPass.Length != 8)
+            {
+                Console.WriteLine("Error el password debe de ser de tamanio 8");
+            }
+
+            else
+            {
+                DataSet ds = new DataSet();
+                // int id_usuario = Convert.ToInt32(ds.Tables[0].Rows[0][0]);
+                ds = op.Crear_Usuario(txtusuario.Text, txtcontra.Text, txtnombre.Text, txtcorreo.Text);
+                Response.Redirect("Default.aspx");
+            }
+        }
+
         protected void registro_click(object sender, EventArgs e)
         {
-            string usuario = txtusuario.Text;
+            /*string usuario = txtusuario.Text;
             string contra = txtcontra.Text;
             string correo = txtcorreo.Text;
             string nombre = txtnombre.Text;
@@ -27,7 +83,9 @@ namespace BancoGT
             {
                 System.Web.HttpContext.Current.Session["usuario"] = usuario;
                 Response.Redirect("Default");
-            }
+            }*/
+            CrearUsuario();
+            
 
         }
 
---
+++ BancoGT/BancoGT/Operaciones.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace BancoGT
+{
+    public class Operaciones
+    {
+        String conexion = "data source=DESKTOP-N6I4NNI\\SQLEXPRESS;Initial Catalog=practica35;Integrated Security=True";
+        SqlConnection con;
+
+        public DataSet consultar_usuario(string usuario, string password, int id_usuario)
+        {
+            DataSet ds = new DataSet();
+            try
+            {
+                con = new SqlConnection(conexion);
+                con.Open();
+                SqlCommand cmd = new SqlCommand("Get_permisos", con);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("@usuario", usuario);
+                cmd.Parameters.AddWithValue("@password", password);
+                cmd.Parameters.AddWithValue("@id_usuario", id_usuario);
+
+                cmd.Connection = con;
+                SqlDataAdapter da = new SqlDataAdapter(cmd);
+                da.Fill(ds);
+                con.Close();
+            }
+            catch (Exception ex)
+            {
+
+            }
+            return ds;
+        }
+
+        public DataSet Crear_Usuario(String usuario, String contrasena, string nombre, string correo)
+        {
+            DataSet ds = new DataSet();
+            try
+            {
+                con = new SqlConnection(conexion);
+                con.Open();
+                SqlCommand cmd = new SqlCommand("Crear_Usuario", con);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("@usuario", usuario);
+                cmd.Parameters.AddWithValue("@password", contrasena);
+                cmd.Parameters.AddWithValue("@nombre", nombre);
+                cmd.Parameters.AddWithValue("@correo", correo);
+                cmd.Connection = con;
+                SqlDataAdapter datapter = new SqlDataAdapter(cmd);
+                datapter.Fill(ds);
+                con.Close();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error, el usuario no se ha podido crear");
+            }
+
+            return ds;
+        }
+    }
+}
