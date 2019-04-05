fa6563cc31b039ec6463bc22b50e0be65dfebb93
201504242 <201504242@ingenieria.usac.edu.gt>
2019-04-04 20:45:46 -0600

    Registrar datos

--- BancoGT/BancoGT/Operaciones.cs
+++ BancoGT/BancoGT/Operaciones.cs
@@ -9,7 +9,10 @@ namespace BancoGT
 {
     public class Operaciones
     {
-        String conexion = "data source=DESKTOP-N6I4NNI\\SQLEXPRESS;Initial Catalog=practica35;Integrated Security=True";
+        //la tuya
+        //String conexion = "data source=DESKTOP-N6I4NNI\\SQLEXPRESS;Initial Catalog=practica35;Integrated Security=True";
+        //esta es la mia
+        String conexion = "data source=LAPTOP-L3B97VHK\\SQLEXPRESS;Initial Catalog=practica35;Integrated Security=True";
         SqlConnection con;
 
         public DataSet consultar_usuario(string usuario, string password, int id_usuario)
@@ -44,7 +47,7 @@ namespace BancoGT
             {
                 con = new SqlConnection(conexion);
                 con.Open();
-                SqlCommand cmd = new SqlCommand("Crear_Usuario", con);
+                SqlCommand cmd = new SqlCommand("Crear_Usuario2", con);
                 cmd.CommandType = CommandType.StoredProcedure;
                 cmd.Parameters.AddWithValue("@usuario", usuario);
                 cmd.Parameters.AddWithValue("@password", contrasena);
